7552c3c99ca3bb1e7f633d0cc7a8e014d2b721e5
Ronen Gol <ronengol@gmail.com>
2013-01-16 20:33:26 +0200

    Blog Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GolBlog::Application.config.secret_token = 'e2c06feafa97de51f2b97a00713c136615d21136a1f4dab79e7551859d7c5ab1671b30083a1d155a743970ed7115806e24aa9cf6966daeeb3a67e630d7a7ae15'
