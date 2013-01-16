da35b644c17206cfe7d9a1371ae4d18beb68fbff
Ronen Gol <ronengol@gmail.com>
2013-01-16 22:03:37 +0200

    depenent syntax fix

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-  has_many :comments :dependent => :destroy
+  has_many :comments, :dependent => :destroy
   attr_accessible :body, :title, :user_id
 end
