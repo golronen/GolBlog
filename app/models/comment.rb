91d5ac8b271f3f4d1dcd16df866f3fa7c46a9eeb
Ronen Gol <ronengol@gmail.com>
2013-05-28 20:06:24 -0700

    try

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
   attr_accessible :body, :commenter, :post_id
+  validates :body, :length => { :maximum => 250 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments, :dependent => :destroy
   attr_accessible :body, :title, :user_id
+  validates :title, :length => { :maximum => 200 }
 end
